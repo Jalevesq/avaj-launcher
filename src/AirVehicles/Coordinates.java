9538ced67739a0baa2a3664bb9023c6a944987a2
Jalevesq <jacob25.levesque1@outlook.com>
2024-02-09 10:51:59 -0500

    coordinate class

--- src/AirVehicles/Coordinates.java
+++ src/AirVehicles/Coordinates.java
@@ -1,5 +1,25 @@
 package AirVehicles;
 
 public class Coordinates {
-    
+    private int longitude_;
+    private int latitude_;
+    private int height_;
+
+    public Coordinates(int longitude, int latitude, int height) {
+        this.height_ = height;
+        this.latitude_ = latitude;
+        this.longitude_ = longitude;
+    }
+
+    public int getLongitude() {
+        return this.longitude_;
+    }
+
+    public int getLatitude() {
+        return this.latitude_;
+    }
+
+    public int getHeight() {
+        return this.height_;
+    }
 }
