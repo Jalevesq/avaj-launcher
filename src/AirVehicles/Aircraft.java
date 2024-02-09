f4c75161832238cd65667185fd76c964f5547c16
Jalevesq <jacob25.levesque1@outlook.com>
2024-02-09 11:07:12 -0500

    small change

--- src/AirVehicles/Aircraft.java
+++ src/AirVehicles/Aircraft.java
@@ -1,5 +1,18 @@
 package AirVehicles;
 
 public class Aircraft {
+    protected long id_;
+    protected String name_;
+    protected Coordinates coordinates_;
+    private static long idCounter_ = 0;
     
+    protected Aircraft(String name, Coordinates coordinates) {
+        this.name_ = name;
+        this.coordinates_ = coordinates;
+        this.id_ = nextId();
+    }
+
+    private static long nextId() {
+        return idCounter_++;
+    }
 }
