6c6075ddb77d679b66f57a904a8ff9f77238edc3
Jalevesq <jacob25.levesque1@outlook.com>
2024-02-10 11:00:51 -0500

    add comment

--- src/app/App.java
+++ src/app/App.java
@@ -5,7 +5,12 @@ import controlcenter.Flyable;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Flyable somethingIsFlying = AircraftFactory.newAircraft("Helicopter", "Fat momma", 5, 0, 0);
+        // Parsing file:
+        // Check that all number are positive.
+        // Height can't be higher than 100.
+        // First number = number of time to run simulation.
+        // Respect form: TYPE NAME LONGITUDE LATITUDE HEIGHT
+        Flyable somethingIsFlying = AircraftFactory.newAircraft("Helicopter", "test", 5, 0, 0);
 
         System.out.println(somethingIsFlying.getFlyableInfo());
     }
